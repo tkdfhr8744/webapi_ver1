6a76b4cc0a300e8ebfb4cf315a547e15c846a558
tkdfhr8744 <tkdfhr8744@naver.com>
2019-02-08 14:06:13 +0900

    커미잇~

---
+++ Solution_web/WebApplication/Controllers/DataController1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using WebApplication.Modules;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication.Controllers
+{
+    public class DataController1 : Controller
+    {
+        ArrayList list;
+        // GET: api/<controller>
+        [Route("api/select")]
+        [HttpGet]
+        public ArrayList select()
+        {
+            list = new ArrayList();
+
+            Database db = new Database();
+            MySqlDataReader mdr = db.GetReader("select * from Notice");
+
+            while (mdr.Read())
+            {
+                Hashtable ht = new Hashtable();
+                for(int i = 0; i < mdr.FieldCount; i++)
+                {
+                    ht.Add(mdr.GetName(i), mdr.GetValue(i));
+                }
+                list.Add(ht);
+            }
+            mdr.Close();
+            db.connclose();
+            return list;
+        }
+        
+    }
+}
---
+++ Solution_web/WebApplication/Modules/Database.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Modules
+{
+    public class Database
+    {
+        private MySqlConnection conn;
+
+        private MySqlConnection GetConnection()
+        {
+            conn = new MySqlConnection();
+            conn.ConnectionString = string.Format("server={0};user={1};password={2};database={3};port={4}", "gdc3.gudi.kr", "root", "1234", "gdc", "22002");
+            try
+            {
+                conn.Open();
+                return conn;
+            }
+            catch
+            {
+                return null;
+            }
+            
+        }
+        public void connclose()
+        {
+            conn.Close();
+        }
+        public MySqlDataReader GetReader(string sql)
+        {
+            try
+            {
+                MySqlCommand comm = new MySqlCommand();
+                GetConnection();
+                comm.CommandText = sql;
+                comm.Connection = conn;
+                MySqlDataReader mdr = comm.ExecuteReader();
+                return mdr;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+  
+}
